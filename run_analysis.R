2ff4781b4b161fb9479b254d75cae9840dbc8625
robatod <rdewitt@gmail.com>
2016-04-19 00:07:50 +0800

    Final upload including step 5

--- run_analysis.R
+++ run_analysis.R
@@ -39,13 +39,14 @@ x_y_df <- rbind(x_y_test_df, x_y_train_df)
 # 2. Extracts only the measurements on the mean and standard deviation for each measurement.
 cols_mean_std <- grep("[Mm]ean|[Ss]td",names(x_y_df), value = TRUE)
 # don't drop the activity column
-cols_mean_std <- c("activity",cols_mean_std)
-x_y_df <- x_y_df[,cols_mean_std]
+x_y_df <- x_y_df[,c("activity",cols_mean_std)]
 
 # 3. Uses descriptive activity names to name the activities in the data set
 activity_labels_file <- "activity_labels.txt"
 activity_labels_df <- read.delim(activity_labels_file, sep="", header = FALSE, stringsAsFactors = FALSE, col.names = c("num","description"))
 x_y_df <- mutate(x_y_df, activity= activity_labels_df[x_y_df$activity,2])
 
+x_y_by_activity_df <- group_by(x_y_df, activity)
+x_y_activity_summary <- summarise_each(x_y_by_activity_df,funs(mean))
 
 
